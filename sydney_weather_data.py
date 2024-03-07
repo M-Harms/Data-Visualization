2c5d3d92cb26ea7dafaf83df493e7e0902530849
Highdeltav <highdeltav@protonmail.com>
2024-03-06 16:07:45 -0800

    Changes

--- sydney_weather_data.py
+++ sydney_weather_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 def load_data():
@@ -14,9 +15,20 @@ def load_data():
     # Convert date column to a datetime object
     df['Date'] = pd.to_datetime(df['Date'])
 
-    # Add a month column
+    # Add a month and season column
     df['month'] = df.Date.dt.month
 
+    conditions = [
+        (df['month'] == 12),
+        ((1 <= df['month']) & (df['month'] < 3)),
+        ((3 <= df['month']) & (df['month'] < 6)),
+        ((6 <= df['month']) & (df['month'] < 9)),
+        ((9 <= df['month']) & (df['month'] < 12))
+    ]
+    outputs = ['Summer', 'Summer', 'Fall', 'Winter', 'Spring']
+
+    df['season'] = pd.Categorical(pd.Series(np.select(conditions, outputs, None)).values
+                                  , ['Spring', 'Summer', 'Fall', 'Winter'])
     # Convert the boolean, yes/no columns into 1s and 0s
     df.loc[df['RainToday'] == 'Yes', 'RainToday'] = 1
     df.loc[df['RainToday'] == 'No', 'RainToday'] = 0
@@ -30,7 +42,6 @@ def load_data():
 
     # Drop rows with NA Values
     # Drop RainTomorrow, since it is supposed to be truth
-    # df.dropna(subset='RainTomorrow', inplace=True)
     df.dropna(subset='WindDir9am', inplace=True)
     df.dropna(subset='WindDir3pm', inplace=True)
     df.dropna(subset='Cloud9am', inplace=True)
@@ -46,8 +57,5 @@ def load_data():
                          'Pressure9am', 'Pressure3pm', 'Temp9am', 'Temp3pm', 'RainToday']
     for col in columns_to_median:
         df[col].fillna(df.groupby(['month'])[col].transform('median'), inplace=True)
-    #df['WindGustDir'].fillnna('None')
-    # Changing WindGustDir to use categories. Also makes the null values equal to zero, which is expected
-    #df['WindGustDir'] = df['WindGustDir'].astype('category').cat.codes
 
     return df
